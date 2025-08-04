5f8757f150cb5a4706064a167c7414e4a5883984
Monzirmajdi <bob121953@gmail.com>
2025-08-04 13:26:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 role: "Content Creator",
                 tools: "Photoshop, Canva",
                 description: "Designed a series of engaging posts for Instagram and Facebook.",
-                previewImage: "images/Social-media/Untitled81_20250804125939.png",
+                previewImage: "images/Social-media/Untitled83_20250804132250.png",
                 detailImage: "images/Social-media/Untitled80_20250804071524.webp"
             },
             {
